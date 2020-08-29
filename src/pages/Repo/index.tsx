a400cfd0bbb89654f89f67c09f4c388ddcfab57f
xpedroleonardo <aispedroleonardo@gmail.com>
2020-08-29 20:23:06 -0300

    Acessar repositório

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,39 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 import { Container, Breadcrumb, RepoIcon, Stats, StarIcon, ForkIcon, LinkButton, GithubIcon } from './styles';
 
+import { APIRepo } from '../../@types';
+
+interface Data {
+  repo?: APIRepo;
+  error?: string;
+}
+
 const Repo: React.FC = () => {
+
+  const { username, reponame } = useParams();
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+    fetch(`https://api.github.com/repos/${username}/${reponame}`).then(
+      async (response) => {
+        setData(
+          response.status === 404
+            ? { error: 'Repository not found!' }
+            : { repo: await response.json() }
+        );
+      }
+    );
+  }, [reponame, username]);
+
+  if (data?.error) {
+    return <h1>{data.error}</h1>;
+  }
+
+  if (!data?.repo) {
+    return <h1>Loading...</h1>;
+  }
+
+
   return (
     <Container>
       <Breadcrumb>
         <RepoIcon />
-        <Link className={'username'} to={'/xpedroleonardo'}>
-          xpedroleonardo
+        <Link className={'username'} to={`/${username}`}>
+          {username}
         </Link>
         <span>/</span>
-        <Link className={'reponame'} to={'/xpedroleonardo/Twitter-UI-Clone'}>
-          Twitter-UI-Clone
+        <Link className={'reponame'} to={`/${username}/${reponame}`}>
+          {reponame}
         </Link>
       </Breadcrumb>
 
-      <p>sadfughnafdkjfghvnasdfkiujghasdfjguhhbnukjmhnk</p>
+      <p>{data.repo.description}</p>
 
 
       <Stats>
         <li>
           <StarIcon />
-          <b>0</b>
+          <b>{data.repo.stargazers_count}</b>
           <span>stars</span>
         </li>
         <li>
           <ForkIcon />
-          <b>0</b>
+          <b>{data.repo.forks}</b>
           <span>forks</span>
         </li>
       </Stats>
 
-      <LinkButton href={'https://github.com/xpedroleonardo/Twitter-UI-Clone'} target="_blank">
+      <LinkButton href={`https://github.com/${username}/${reponame}`} target="_blank">
         <GithubIcon />
         <span>View on GitHub</span>
       </LinkButton>
